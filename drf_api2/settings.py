1cc1571356269f897d4cce885f7f585d61465243
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-10-16 12:22:08 +0200

    Create requirements.txt file with my dependencies, Update profile model, register the profile model

--- drf_api2/settings.py
+++ drf_api2/settings.py
@@ -19,7 +19,7 @@ if os.path.exists('env.py'):
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
