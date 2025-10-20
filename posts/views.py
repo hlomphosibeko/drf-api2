c19fc9c42316ca5e9fc9114710809ccb61851df2
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-10-20 14:27:57 +0200

    Create PostDetail view to retrieve, update and delete post if owner

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 # from django.db.models import Count
 # from rest_framework import generics, permissions, filters
 # from django_filters.rest_framework import DjangoFilterBackend
-# from lins_api.permissions import IsOwnerOrReadOnly
+from django.http import Http404
+from drf_api2.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 from rest_framework import status, permissions
@@ -33,4 +34,43 @@ class PostList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class PostDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PostSerializer
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
