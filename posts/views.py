cad7fe6682bd727f377f57ae128c31cd41253815
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-10-20 12:58:32 +0200

    Create PostList view, define get method to list posts and post method to create post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+# from django.db.models import Count
+# from rest_framework import generics, permissions, filters
+# from django_filters.rest_framework import DjangoFilterBackend
+# from lins_api.permissions import IsOwnerOrReadOnly
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class PostList(APIView):
+    serializer_class = PostSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+        ]
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
