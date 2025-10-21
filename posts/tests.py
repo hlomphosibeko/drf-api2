81ddab9752cd30108f67a55a4d9afe6a2083f4fd
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-10-21 11:13:03 +0200

    Create comments app, Add serializers, Register app in admin.py, Add app to installed apps list

--- posts/tests.py
+++ posts/tests.py
@@ -24,3 +24,38 @@ class PostListViewTests(APITestCase):
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/posts/', {'title': 'a title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        olaf = User.objects.create_user(username='olaf', password='pass')
+        elsa = User.objects.create_user(username='elsa', password='pass')
+        Post.objects.create(
+            owner=olaf, title='a title', content='olafs content'
+        )
+        Post.objects.create(
+            owner=elsa, title='another title', content='elsas content'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    # def test_user_can_update_own_post(self):
+    #     user = User.objects.get(username='olaf')
+    #     self.client.force_authenticate(user=user)
+    #     response = self.client.put('/posts/1/', {'title': 'a new title'})
+    #     post = Post.objects.filter(pk=1).first()
+    #     self.assertEqual(post.title, 'a new title')
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_user_cant_update_another_users_post(self):
+    #     user = User.objects.get(username='olaf')
+    #     self.client.force_authenticate(user=user)
+    #     response = self.client.put('/posts/2/', {'title': 'a new title'})
+    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
