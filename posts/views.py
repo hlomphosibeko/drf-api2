6a582dfca4a8505f7b1451be999e60c3edcd25ea
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-10-23 14:25:56 +0200

    App post and profile filters using DjangoFilterBackend

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
-# from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api2.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -22,7 +22,7 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
-        # DjangoFilterBackend,
+        DjangoFilterBackend,
     ]
     filterset_fields = [
         'owner__followed__owner__profile',
