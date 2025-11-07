201ab00a75ff12a00941a0bbaad2460cc33d2c5a
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-11-07 12:16:50 +0200

    Prepare the Django API for development

--- env.py
+++ env.py
@@ -1,7 +1,9 @@
 import os
 os.environ['CLOUDINARY_URL'] = 'cloudinary://992948875945733:HL7ks05pQk4NynB_yt4dYe15-eU@dgrfqu1ca'
-os.environ['DEV'] = '1'
+# os.environ['DEV'] = '1'
 os.environ['DATABASE_URL'] = "postgresql://neondb_owner:npg_vOt6cPJV3Uab@ep-damp-queen-agxzpbu4.c-2.eu-central-1.aws.neon.tech/blast_cape_shown_886168"
 os.environ.setdefault('SECRET_KEY', 'vPk~oZS>s(;@(VN')
 os.environ['SECRET_KEY'] = '7Nd4M34kCG'
-os.environ['ALLOWED_HOST'] = 'drf-api-2-c591d8e57ab2.herokuapp.com'
+os.environ['ALLOWED_HOST'] = '127.0.0.1'
+os.environ['DEBUG'] = '1'
+os.environ['CLIENT_ORIGIN'] = 'http://127.0.0.1'
