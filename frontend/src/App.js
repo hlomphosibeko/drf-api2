b8f86e942f9f46978c497aca7753a173782edef4
hlomphosibeko <hlomphosibeko2@gmail.com>
2025-11-10 20:59:12 +0200

    Create NavBar.js file and update it, Add logo and favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
